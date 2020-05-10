7d7edd0422003685567bfe4eb9e59b1d45368316
Daniel Silverstone <dsilvers@digital-scurf.org>
2020-05-10 16:36:40 +0100

    test-template: Add testing for no-std support
    Signed-off-by: Daniel Silverstone <dsilvers@digital-scurf.org>

--- test-template/src/main.rs
+++ test-template/src/main.rs
@@ -1,7 +1,23 @@
+#[cfg(not(feature = "no-std"))]
 use git_testament::{git_testament, render_testament};
 
+#[cfg(not(feature = "no-std"))]
 git_testament!(TESTAMENT);
 
+use git_testament::git_testament_macros;
+
+git_testament_macros!(version, "trusted");
+
+#[cfg(not(feature = "no-std"))]
 fn main() {
+    assert_eq!(
+        format!("{}", render_testament!(TESTAMENT, "trusted")),
+        version_testament!()
+    );
     println!("{}", render_testament!(TESTAMENT, "trusted"));
+}
+
+#[cfg(feature = "no-std")]
+fn main() {
+    println!("{}", concat!("", version_testament!()));
 }
